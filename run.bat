f58e5ad364dfc7d61e4eeeb84965eeafeb0fe562
Sayan2313 <sayansinha728@gmail.com>
2025-12-16 13:49:45 +0530

    played

--- run.bat
+++ run.bat
@@ -1,2 +1,1 @@
-java -Xms6G -Xmx8G -jar fabric-server-mc.1.21.11-loader.0.18.2-launcher.1.1.0.jar nogui
-pause
+java -Xms4G -Xmx6G -jar fabric-server-mc.1.21.11-loader.0.18.2-launcher.1.1.0.jar nogui
