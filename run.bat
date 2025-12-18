e7fd1b85972141a180432fff2eed8c07240c170c
Sayan2313 <sayansinha728@gmail.com>
2025-12-18 19:06:45 +0530

    update server files

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
-java -Xms4G -Xmx6G -jar fabric-server-mc.1.21.11-loader.0.18.2-launcher.1.1.0.jar nogui
+java -Xms4G -Xmx6G -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:+ParallelRefProcEnabled -jar fabric-server-mc.1.21.11-loader.0.18.3-launcher.1.1.0.jar nogui
+PAUSE
